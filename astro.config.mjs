407739cfa54a631d87892c7895fea7597e29264c
sonplusone <son.nguyen@plusonerobotics.com>
2023-02-04 11:56:35 -0600

    Set up base project with react and tailwindcss

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+// https://astro.build/config
+import { defineConfig } from "astro/config";
+
+// Framework integrations
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+
+export default defineConfig({
+  integrations: [react(), tailwind()],
+});
